8b31954f1e74ebfd22b404eaf1d4278fff06c39b
risaruno <38074029+risaruno@users.noreply.github.com>
2022-03-07 16:38:29 +0900

    "initiate commit"

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// document.getElementById("open").addEventListener("click", () => {
+//   let landing = document.getElementById("landing");
+//   landing.style.opacity = "0";
+//   open = () => {
+//     landing.remove();
+//   };
+//   setTimeout(() => {
+//     open();
+//   }, 1000);
+// });
+var i = 9;
+var a = () => {
+  const b = [1, 2, 3];
+  console.log(b.length);
+};
