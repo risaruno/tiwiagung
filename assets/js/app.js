a506968d5eb34c19324d803e4bd8f8075a5cbf78
Rizal Arief <risarunomamoru@gmail.com>
2022-03-08 17:42:46 +0900

    Installing Gsap ScrollTo NEVER USE scrollTo()

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,29 @@
 //     open();
 //   }, 1000);
 // });
-var i = 9;
-var a = () => {
-  const b = [1, 2, 3];
-  console.log(b.length);
-};
+
+
+// Scroll To
+var timesection = [];
+var navItems = document.querySelectorAll("#navigation>ul>li");
+navItems.forEach((item, index) => {
+  var target = item.getAttribute("data-target");
+  item.addEventListener("click", scroll);
+  
+  // Navigation Active Toggle
+  timesection[index] = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#" + target,
+      start: "top center",
+      endTrigger: "#" + target,
+      end: "bottom center",
+      toggleClass: { targets: item, className: "active" },
+    },
+  });
+
+
+});
+function scroll() {
+  var target = this.getAttribute("data-target");
+  gsap.to(window, 1, { scrollTo: "#" + target, ease: "power2.inOut" });
+}
